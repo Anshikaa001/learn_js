5542508332ad6a3909fb181f5fca397b651d822a
Anshika Kansal <anshikakansal001@gmail.com>
2025-01-10 21:01:34 +0000

    conversion of data types

---
+++ 01_basics/conversionofdatatypes.js
@@ -0,0 +1,34 @@
+let score = "anshika"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+
+let ValueInNumber = Number(score)
+console.log(typeof ValueInNumber);
+console.log(ValueInNUmber)
+
+// "33" => 33
+// "33abc" => Nan( not a number)
+// true=1, false=0
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//  1 => true, 0 => false
+// "" => false
+// "anshika" => true
+
+
+let someNumber = 33
+
+let stringNumber = string(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
