b2cf25b9f6ab8ccded2c332b22c4caa548806a4a
Anshika Kansal <anshikakansal001@gmail.com>
2025-01-12 09:20:16 +0000

    data types

---
+++ 01_basics/3datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) //this wont work on nodejs, it works on browser
+
+let name = "anshika"
+let age = 18;
+let isLoggedIn = true
+
+// number => 2 to power 53
+// bigint
+// string => ""
+//boolean => true/false
+// null => stand alone value or empty value
+// undefined => value is nor assigned yet
+// symbol => to define uniqueness
+
+//object
+
+
+
+console.log(typeof age); //number
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
+
+
