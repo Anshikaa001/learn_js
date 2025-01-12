6d06dd25d9e4acf5e64938a7f93511af8fcca579
Anshika Kansal <anshikakansal001@gmail.com>
2025-01-12 09:20:01 +0000

    variables

---
+++ 01_basics/2variables.js
@@ -0,0 +1,17 @@
+const accountId = 144563
+let accountEmail = "anshika@gmail.com"
+var accountPassward = "12345"
+accountCity = " jaipur"
+let accountState; 
+
+// accountId = 2 not allowed
+// dont use var ever because of issue in block scope
+
+
+accountEmail = "ak@gmail.com"
+accountPassward = "122323"
+
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassward, accountCity, accountState])
