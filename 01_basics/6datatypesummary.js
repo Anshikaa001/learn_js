f92cf273b5739d2d0aa1584183e583fa435d0427
Anshika Kansal <anshikakansal001@gmail.com>
2025-01-15 17:54:24 +0000

    summary of data types

---
+++ 01_basics/6datatypesummary.js
@@ -0,0 +1,33 @@
+//data types are classified on the basis of how data is stored and accessed in memory
+
+
+//    primitive type
+
+// 7 types: string, number, boolean, null, undefined, symbol, bigInt,
+
+
+//   Refrence type or non primitive
+// inka refrence is directly allocated
+
+// types: Arrays, objects, functions
+
+const heros = ["xmen", "batman", "iron man"] //array
+
+let myObj = {
+    name :"anshika", //curly braces me is objects
+    age: 22
+
+}
+
+
+const myFunction = function(){
+    console.log("hello world");
+    
+}
+
+
+// Javascript is dynamically typed language
+
+// all data types ki return value is the data type only 
+// null ka return data type is  object 
+// and function ka object function
