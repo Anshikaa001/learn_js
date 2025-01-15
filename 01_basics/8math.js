49dcd2a8696a0047b38106afc96ecece5899ba0b
Anshika Kansal <anshikakansal001@gmail.com>
2025-01-15 18:21:53 +0000

    numbers and math

---
+++ 01_basics/8math.js
@@ -0,0 +1,11 @@
+const score = 100 //100
+const score2 = new Number(200)  //Number: 200
+
+console.log(score.toFixed(1)); 
+console.log(score.toLocalString('en-IN')); 
+
+
+// ***********maths********
+
+// methods => check mdn
+// Math.random => gives value between 0 to 1
