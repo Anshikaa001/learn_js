fe9760fccc7d70463b0707494947e788ec6b691f
Anshika Kansal <anshikakansal001@gmail.com>
2025-01-15 19:18:42 +0000

    date and time

---
+++ 01_basics/9dates.js
@@ -0,0 +1,13 @@
+let date = new Date()
+console.log(date);
+
+
+
+console.log(date.toDateString());
+console.log(typeof date); //object asked in interviews
+
+// months start from 0 in javascript
+
+
+
+
