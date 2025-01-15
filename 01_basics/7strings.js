903fc1ea6e292fd95089c11e00d400d025e141b5
Anshika Kansal <anshikakansal001@gmail.com>
2025-01-15 17:54:41 +0000

    strings in js

---
+++ 01_basics/7strings.js
@@ -0,0 +1,31 @@
+const user  = "anshika"
+const repoCount = 20
+
+console.log("My user is ${user} and my repo count is ${repoCount} ");
+
+
+// declaring string
+const nam  = "daksh"
+const gameName = new String ('vallorant')
+
+// string is an object here
+
+
+// methods used with string:
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.CharAt(2));
+console.log(gameName.indexOf('t'));
+
+const newtring = "    Anshika   "
+console.log(newtring.trim());
+
+
+const url = "https://year.com/newyear2025"
+console.log( url.replace('2025', '2026'));
+
+//to covert a string to Array 
+const fruit = "apple-mango-papaya"
+console.log(fruit.split('-')); //"apple, "mango, "papaya"
+
+
