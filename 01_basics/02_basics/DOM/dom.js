34362148db3ab1ca66e176f0f8473ecb1ed977bf
Anshika Kansal <anshikakansal001@gmail.com>
2025-01-20 14:33:16 +0000

    ddocument object model

---
+++ 01_basics/02_basics/DOM/dom.js
@@ -0,0 +1,7 @@
+// window object => it is an open window in browser. it is automatically made by browser. it contains many properties and methods
+
+
+// dom= document object model
+// an object called document is created in windows object. 
+// we can access our whole html code from this document oject.
+// we can make dynamic changes using dom
